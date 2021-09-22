28f253d6dd5c1f49217507c71997825491bb374c
Phillip Jackson <unisys12@gmail.com>
2021-09-22 06:45:19 -0500

    setup rough environment

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/unisys12/destinycli
 
 go 1.17
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Eye's up guardian!")
+
+	// Environment Setup should probably be moved to an Init method
+	// or project scaffold. Just thinking outload.
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	BUNKEY := os.Getenv("X-API-KEY")
+
+	message := fmt.Sprintf("Using API Key: %v", BUNKEY)
+
+	fmt.Println(message)
 }
